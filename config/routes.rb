ca091a531176fff5e8584c739299fc379aad4610
sane yaan <ec2-user@ip-172-31-1-142.us-west-2.compute.internal>
2022-07-28 20:02:22 +0000

    addfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/hello'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :users
   # Defines the root path route ("/")
   # root "articles#index"
 end
